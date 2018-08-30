281164e9282b6bbc1cbd9e10fa880d978349702e
Josiah Phipps <jphipps@trialcard.com>
2018-08-30 10:56:53 -0400

    works

--- index.js
+++ index.js
@@ -91,6 +91,27 @@ controller.on('rtm_close', function (bot) {
  */
 var billQuotes = [];
 
+base('Quotes').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 300,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function (record) {
+        billQuotes.push(record.get('quote'));
+        console.log('Retrieved', record.get('Name'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {        
+    if (err) { console.error(err); return; }    
+});
+
 router.post('/quote', (req, res) => {
     if (req.body.quote) {
         base('Quotes').create({
